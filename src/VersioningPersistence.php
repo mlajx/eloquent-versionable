fb2a79f43a833e349ea359c09d82794a65a660f2
Marcelo Araujo Jr <marcelo.araujo@cohros.com.br>
2022-02-10 15:36:19 -0200

    updated versioning

--- src/VersioningPersistence.php
+++ src/VersioningPersistence.php
@@ -13,9 +13,12 @@ class VersioningPersistence
 
     public static function updateNextColumnOfLastVersionedRegister(Model $model)
     {
-        $lastVersioned = self::getVersionedModel($model)->where('id', $model->id)
+        $lastVersioned = self::getVersionedModel($model)
+            ->withoutGlobalScopes()
+            ->where('id', $model->id)
             ->orderBy('_id', 'desc')
             ->first();
+
         $lastVersioned->timestamps = false;
 
         $lastVersioned->update(['next' => $model->{$model->getUpdatedAtColumn()}]);
